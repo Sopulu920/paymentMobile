20c9ae28e6ee1543fd868d3b7a23eb2d084f445a
Sopulu920 <michaelmorah91@gmail.com>
2025-08-04 00:13:13 +0100

    [ADD] tab route

--- App.tsx
+++ App.tsx
@@ -1,15 +1,35 @@
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Home from "./widgets/Home/index";
+import Profile from './widgets/Profile';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName: string;
 
-   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Profile') {
+              iconName = 'user';
+            }
 
-        <Home/>
-
-      </SafeAreaView>
-    </SafeAreaProvider>
+            return <FontAwesome5 name={iconName!} size={size} color={color} solid />;
+          },
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} options={{ title: 'Welcome' }} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
